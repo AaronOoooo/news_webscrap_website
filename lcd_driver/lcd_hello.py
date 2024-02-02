f12cdb479ec10848e3ca634ee0f9bbfa8a3f0741
AaronOoooo <aaron.o.hall@gmail.com>
2024-02-02 17:09:52 -0600

    updated LCD to show date and time to make device more active

--- lcd_driver/lcd_hello.py
+++ lcd_driver/lcd_hello.py
@@ -1,4 +1,20 @@
 import I2C_LCD_driver
-from time import *
+from datetime import datetime
+from time import sleep
+
 mylcd = I2C_LCD_driver.lcd()
-mylcd.lcd_display_string("Good day, Aaron!", 1)
+
+while True:
+    # Get current date and time
+    now = datetime.now()
+    month_abbr = now.strftime("%b")  # Abbreviated month name
+    date_str = f"{month_abbr} {now.day}, {now.year}"
+    time_str = now.strftime("%I:%M:%S %p")  # 12-hour time format with seconds and AM/PM
+
+    # Update LCD display
+    mylcd.lcd_clear()
+    mylcd.lcd_display_string(date_str.center(16), 1)
+    mylcd.lcd_display_string(time_str.center(16), 2)
+
+    # Sleep for a while before updating again
+    sleep(1)
